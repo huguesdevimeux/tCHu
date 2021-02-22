4dbfd50bf3b691cac205afb41385b0bd076abc1c
lucamouchel <lucamouchel13@gmail.com>
2021-02-22 20:59:33 +0100

    modif

--- src/ch/epfl/tchu/Preconditions.java
+++ src/ch/epfl/tchu/Preconditions.java
@@ -2,9 +2,13 @@ package ch.epfl.tchu;
 
 public final class Preconditions {
     private Preconditions() {
-        //TODO complete
+        //no possible instantiations
     }
 
+    /*
+     * throws IllegalArgumentException parameter is false
+     * @param shouldBeTrue Expression to check.
+     */
     public static void checkArgument(boolean shouldBeTrue) {
         if (!shouldBeTrue) throw new IllegalArgumentException();
     }
