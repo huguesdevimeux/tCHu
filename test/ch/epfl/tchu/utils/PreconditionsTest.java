5c24a131672008aa0e238189cb9f1c52f71606b0
Hugues Devimeux <thespourieye@gmail.com>
2021-02-22 18:45:51 +0100

    added tests

---
+++ test/ch/epfl/tchu/utils/PreconditionsTest.java
@@ -0,0 +1,20 @@
+// Rigel stage 1
+
+package ch.epfl.tchu.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PreconditionsTest {
+    @Test
+    void checkArgumentSucceedsForTrue() {
+        Preconditions.checkArgument(true);
+    }
+
+    @Test
+    void checkArgumentFailsForFalse() {
+        assertThrows(IllegalArgumentException.class, () -> Preconditions.checkArgument(false));
+    }
+}
