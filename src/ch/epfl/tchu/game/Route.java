2331460756800d7d4e7fa6758bbb18f54a4b826a
luca mouchel <73081373+lucamouchel@users.noreply.github.com>
2021-03-04 14:40:26 +0100

    [2 3.4]WIP route class (#16)
    * [2 3.4]WIP route class
    
    * format
    
    * [1 all] added @author (#15)
    
    Co-authored-by: lucamouchel <lucamouchel13@gmail.com>
    
    * [2 3.(1,2,3)] added provided classes - to not modify (#13)
    
    * [2 3.(1,2,3)] added provided classes - to not modify
    
    * format
    
    Co-authored-by: lucamouchel <lucamouchel13@gmail.com>
    Co-authored-by: github-actions <>
    
    * [2 3.4]WIP route class
    
    * format
    
    * added Tests for Route
    
    * format
    
    * Update src/ch/epfl/tchu/game/Route.java
    
    Co-authored-by: Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>
    
    * Update src/ch/epfl/tchu/game/Route.java
    
    Co-authored-by: Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>
    
    * Update src/ch/epfl/tchu/game/Route.java
    
    Co-authored-by: Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>
    
    * Update src/ch/epfl/tchu/game/Route.java
    
    Co-authored-by: Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>
    
    * Update src/ch/epfl/tchu/game/Route.java
    
    Co-authored-by: Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>
    
    * fixed route so that tests work
    
    * format
    
    * added two last methods w/out javadoc
    
    * format
    
    * removed main from Route.java
    
    * added comments
    
    * format
    
    * changed and added tests for Route.java
    
    * format
    
    * little fix
    
    * little fix
    
    * fix
    
    * format
    
    * Update src/ch/epfl/tchu/game/Route.java
    
    Co-authored-by: Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>
    
    * changed tests
    
    * format
    
    * changed tests
    
    * changed var names and added fixed lil javadoc
    
    * Update src/ch/epfl/tchu/game/Route.java
    
    Co-authored-by: Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>
    
    * little fucking fixes argh
    
    * format
    
    * separated certain tests and checked functionality
    
    * format
    
    * testname
    
    Co-authored-by: github-actions <>
    Co-authored-by: Hugues Devimeux <thespourieye@gmail.com>
    Co-authored-by: Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>

---
+++ src/ch/epfl/tchu/game/Route.java
@@ -0,0 +1,249 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.Preconditions;
+import ch.epfl.tchu.SortedBag;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Representation of a route that links two nearby stations.
+ *
+ * @author Luca Mouchel (324748)
+ * @author Hugues Devimeux (327282)
+ */
+public final class Route {
+    private final String id;
+    private final Station station1;
+    private final Station station2;
+    private final int length;
+    private final Level level;
+    private final Color color;
+
+    /** Enum defines the type of route. */
+    public enum Level {
+        OVERGROUND,
+        UNDERGROUND
+    }
+
+    /**
+     * Route constructor.
+     *
+     * @param id Routes' identity
+     * @param station1 First station of the route
+     * @param station2 Second station of the route
+     * @param length Length of the route
+     * @param level Defines what type of route it is
+     * @param color Can be any color
+     * @throws IllegalArgumentException If stations 1 and 2 are the same or if length is out of
+     *     bounds defined by Constants.java
+     * @throws NullPointerException if either id, station1, station2 or level are null
+     */
+    public Route(
+            String id, Station station1, Station station2, int length, Level level, Color color) {
+        this.id = Objects.requireNonNull(id);
+        this.station1 = Objects.requireNonNull(station1);
+        this.station2 = Objects.requireNonNull(station2);
+        this.level = Objects.requireNonNull(level);
+        this.length = length;
+        this.color = color;
+        Preconditions.checkArgument(!(station1.equals(station2)));
+        Preconditions.checkArgument(
+                length >= Constants.MIN_ROUTE_LENGTH && length <= Constants.MAX_ROUTE_LENGTH);
+    }
+
+    /**
+     * Returns routes' id.
+     *
+     * @return id
+     */
+    public String id() {
+        return id;
+    }
+
+    /**
+     * Returns station1.
+     *
+     * @return station1
+     */
+    public Station station1() {
+        return station1;
+    }
+
+    /**
+     * Returns station2.
+     *
+     * @return station2
+     */
+    public Station station2() {
+        return station2;
+    }
+
+    /**
+     * Returns routes' length.
+     *
+     * @return length
+     */
+    public int length() {
+        return length;
+    }
+
+    /**
+     * Returns the routes' level: underground or overground.
+     *
+     * @return level
+     */
+    public Level level() {
+        return level;
+    }
+
+    /**
+     * Returns the routes' color or null if the color is plain.
+     *
+     * @return color
+     */
+    public Color color() {
+        return color;
+    }
+
+    /**
+     * Returns List of the two stations in the order they appear in the constructor.
+     *
+     * @return list of the two stations
+     */
+    public List<Station> stations() {
+        return List.of(station1, station2);
+    }
+
+    /**
+     * Returns the opposite station from which this method is called.
+     *
+     * @param station
+     * @throws IllegalArgumentException if argument station is neither of the start/end stations
+     * @return opposite station
+     */
+    public Station stationOpposite(Station station) {
+        Preconditions.checkArgument((station.equals(station1) || station.equals(station2)));
+        if (station.equals(station1)) {
+            return station2;
+        } else {
+            return station1;
+        }
+    }
+
+    /**
+     * Returns List of all the possible cards one can use to take over a route.
+     *
+     * @return possible claim cards (playable cards)
+     */
+    public List<SortedBag<Card>> possibleClaimCards() {
+        SortedBag.Builder<Card> cardBuilder = new SortedBag.Builder<>();
+        List<SortedBag<Card>> cardList = new ArrayList<>();
+
+        if (level.equals(Level.OVERGROUND)) {
+            // when route is overground locomotive cards cannot be used
+            if (color == null) {
+                for (Card card : Card.CARS) {
+                    for (int i = 0; i < this.length; i++) {
+                        cardBuilder.add(card);
+                    }
+                    /// adding all the cars added to cardBuilder into cardList
+                    cardList.add(cardBuilder.build());
+                    // resetting cardBuilder to prevent from having subArrays of cardList to have
+                    // more
+                    // than lengths' elements
+                    cardBuilder = new SortedBag.Builder<>();
+                }
+            } else {
+                for (int j = 0; j < this.length; j++) {
+                    // if color is assigned, we just add the number of length of the route with the
+                    // given colors
+                    cardBuilder.add(Card.of(this.color));
+                }
+                cardList.add(cardBuilder.build());
+            }
+
+        } else {
+            // if <\code> level </\code> is <\code> UNDERGROUND </\code>, Locomotive cards come into
+            // play
+            if (color == null) {
+                for (int i = this.length; i > 0; i--) {
+                    for (Card card : Card.CARS) {
+                        // same instructions as before
+                        for (int j = 0; j < i; j++) {
+                            cardBuilder.add(card);
+                        }
+                        // adding locomotive cards to complete all the possible claim cards
+                        // when route is a tunnel
+                        while (cardBuilder.size() < length) {
+                            cardBuilder.add(Card.LOCOMOTIVE);
+                        }
+                        cardList.add(cardBuilder.build());
+                        cardBuilder = new SortedBag.Builder<>();
+                    }
+                }
+
+            } else {
+                for (int i = this.length; i > 0; i--) {
+                    for (int j = 0; j < i; j++) {
+                        // same instructions but the color here does not matter
+                        // we just assign the given color
+                        cardBuilder.add(Card.of(this.color));
+                    }
+                    while (cardBuilder.size() < length) {
+                        cardBuilder.add(Card.LOCOMOTIVE);
+                    }
+                    cardList.add(cardBuilder.build());
+                    cardBuilder = new SortedBag.Builder<>();
+                }
+            }
+            // this single for loop allows to add the final subArray in the list with ONLY
+            // locomotive cards
+            for (int i = 0; i < this.length; i++) {
+                cardBuilder.add(Card.LOCOMOTIVE);
+            }
+            cardList.add(cardBuilder.build());
+        }
+        return cardList;
+    }
+
+    /**
+     * Returns the additional amount of cards one must play to take over a route knowing that the
+     * player has played with <code> claimCards </code> and the three cards taken from the stack of
+     * cards are the <code> drawnCards </code>
+     *
+     * @param claimCards cards the player uses to play
+     * @param drawnCards 3 cards drawn from the stack of cards
+     * @throws IllegalArgumentException if the route is not a tunnel
+     * @throws IllegalArgumentException if <\code> drawnCards </\code> does not contain exactly
+     *     three cards
+     * @return number of additional cards the player must play to take over route
+     */
+    public int additionalClaimCardsCount(SortedBag<Card> claimCards, SortedBag<Card> drawnCards) {
+        Preconditions.checkArgument(level.equals(Level.UNDERGROUND));
+        Preconditions.checkArgument(drawnCards.size() == 3);
+        int additionalClaimCards = 0;
+        for (Card drawn : drawnCards) {
+            for (Card claim : claimCards) {
+                // for every card that is drawn, if it is a locomotive or the same color as the
+                // claim card
+                // the additionalClaimCards increases
+                if (drawn.equals(Card.LOCOMOTIVE) || (drawn.equals(claim))) {
+                    additionalClaimCards++;
+                }
+            }
+        }
+        return additionalClaimCards;
+    }
+
+    /**
+     * Returns the number of claimPoints (construction points) that a player gets when taking over a
+     * route. Depends on the routes' length.
+     *
+     * @return claimPoints
+     */
+    public int claimPoints() {
+        return Constants.ROUTE_CLAIM_POINTS.get(length);
+    }
+}
