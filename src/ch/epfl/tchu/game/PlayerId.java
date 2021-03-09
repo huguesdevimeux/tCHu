cd9ae5cabc760c5bd4a0e79e34b5d61a72c82b0c
lucamouchel <lucamouchel13@gmail.com>
2021-03-09 18:07:54 +0100

    fix

--- src/ch/epfl/tchu/game/PlayerId.java
+++ src/ch/epfl/tchu/game/PlayerId.java
@@ -10,9 +10,8 @@ import java.util.List;
  * @author Hugues Devimeux (327282)
  */
 public enum PlayerId {
-    // caracterising player ID by its index in attribute ALL
-    PLAYER_1(0),
-    PLAYER_2(1);
+    PLAYER_1,
+    PLAYER_2;
 
     /**
      * Same function as ALL attribute in other classes. Stocks every element (ie the two players) in
@@ -23,20 +22,13 @@ public enum PlayerId {
     /** Attribute that counts the total amount of elements in ALL. */
     public static final int COUNT = ALL.size();
 
-    private final int playerIdIndex;
-
-    /** Default player id constructor */
-    PlayerId(int playerIdIndex) {
-        this.playerIdIndex = playerIdIndex;
-    }
-
     /**
      * Returns the ID of the next player
      *
      * @return other players' ID
      */
     public PlayerId next() {
-        if (playerIdIndex == 0) return PLAYER_2;
+        if (this.equals(PLAYER_1)) return PLAYER_2;
         else return PLAYER_1;
     }
 }
