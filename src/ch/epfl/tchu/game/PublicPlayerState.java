01218c21a6d25a2c5643c40575e42260eb51a6a7
lucamouchel <lucamouchel13@gmail.com>
2021-03-15 16:33:27 +0100

    4-3.4 - not finished

---
+++ src/ch/epfl/tchu/game/PublicPlayerState.java
@@ -0,0 +1,76 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.Preconditions;
+
+import java.util.List;
+
+/** The type Public player state. */
+public class PublicPlayerState {
+    private final int ticketCount;
+    private final int cardCount;
+    private final List<Route> routes;
+    private final int carCount;
+    private final int claimPoints;
+
+    /**
+     * Instantiates a new Public player state.
+     *
+     * @param ticketCount the ticket count
+     * @param cardCount the card count
+     * @param routes the routes
+     */
+    public PublicPlayerState(int ticketCount, int cardCount, List<Route> routes) {
+        Preconditions.checkArgument(ticketCount >= 0 && cardCount >= 0);
+        this.ticketCount = ticketCount;
+        this.cardCount = cardCount;
+        this.routes = routes;
+        this.claimPoints = Constants.ROUTE_CLAIM_POINTS.get(routes.size());
+        this.carCount = 9;
+    }
+
+    /**
+     * Ticket count int.
+     *
+     * @return the int
+     */
+    public int ticketCount() {
+        return ticketCount;
+    }
+
+    /**
+     * Card count int.
+     *
+     * @return the int
+     */
+    public int cardCount() {
+        return cardCount;
+    }
+
+    /**
+     * Routes list.
+     *
+     * @return the list
+     */
+    public List<Route> routes() {
+        return routes;
+    }
+
+    /**
+     * Car count int.
+     *
+     * @return the int
+     */
+    public int carCount() {
+        return carCount;
+    }
+
+    /**
+     * Claim points int.
+     *
+     * @return the int
+     */
+    public int claimPoints() {
+        return claimPoints;
+    }
+
+}
