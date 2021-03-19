e574dfcac0bc5836df2a0c86bd3cecc49e68d898
lucamouchel <lucamouchel13@gmail.com>
2021-03-19 15:58:45 +0100

    enum turnkind - hard to test i think

---
+++ src/ch/epfl/tchu/game/TurnKind.java
@@ -0,0 +1,19 @@
+package ch.epfl.tchu.game;
+
+import java.util.List;
+
+/**
+ * Represents all the actions a player can take in the game.
+ *
+ * @author Luca Mouchel (324748)
+ * @author Hugues Devimeux (327282)
+ */
+public enum TurnKind {
+    /** Enumeration of all the actions a player can take. */
+    DRAW_TICKETS,
+    DRAW_CARDS,
+    CLAIM_ROUTE;
+
+    /** same function as ALL attribute in Color Card, stocks every element of the enum in a List */
+    public static final List<TurnKind> ALL = List.of(TurnKind.values());
+}
