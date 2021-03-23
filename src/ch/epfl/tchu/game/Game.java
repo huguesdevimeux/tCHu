3e5ee839aa84269116dfce53b75c13497da786f1
lucamouchel <lucamouchel13@gmail.com>
2021-03-23 14:16:55 +0100

    still WIP but if u wanna read

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -4,9 +4,11 @@ import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.gui.Info;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 /** Represents a game of tCHu (aka les Aventuriers du Rail but shhh). */
 public final class Game {
@@ -20,15 +22,16 @@ public final class Game {
             SortedBag<Ticket> tickets,
             Random rng) {
         Preconditions.checkArgument(players.size() == 2 && playerNames.size() == 2);
+
+        GameState gameState = GameState.initial(tickets, rng);
+
         players.forEach((playerId, player) -> player.initPlayers(playerId, playerNames));
         PlayerId firstPlayer = PlayerId.ALL.get(rng.nextInt(PlayerId.ALL.size()));
 
         Info currentPlayer = new Info(firstPlayer.name());
         Info nextPlayer = new Info(firstPlayer.next().name());
 
-        players.forEach(
-                (playerId, player) ->
-                        player.receiveInfo(new Info(firstPlayer.name()).willPlayFirst()));
+        players.forEach((playerId, player) -> player.receiveInfo(currentPlayer.willPlayFirst()));
         players.forEach((playerId, player) -> player.setInitialTicketChoice(tickets));
         players.forEach((playerId, player) -> player.chooseInitialTickets());
 
@@ -37,60 +40,97 @@ public final class Game {
                         player.receiveInfo(
                                 new Info(playerId.name())
                                         .keptTickets(
-                                                tickets.size()
+                                                // tickets.size()? or:
+                                                Constants.INITIAL_TICKETS_COUNT
                                                         - player.chooseInitialTickets().size())));
 
         for (Map.Entry<PlayerId, Player> player : players.entrySet()) {
-            player.getValue().receiveInfo(currentPlayer.canPlay());
-            Player playerValue = player.getValue();
 
-            switch (player.getValue().nextTurn()) {
+            Player playerValue = player.getValue();
+            playerValue.receiveInfo(currentPlayer.canPlay());
+            CardState cardState = CardState.of(Deck.of(Constants.ALL_CARDS, new Random()));
+
+            switch (playerValue.nextTurn()) {
                 case DRAW_TICKETS:
                     playerValue.receiveInfo(
                             currentPlayer.drewTickets(Constants.IN_GAME_TICKETS_COUNT));
 
-                    playerValue.chooseTickets(tickets);
-                    playerValue.receiveInfo(
-                            currentPlayer.keptTickets(
-                                    player.getValue().chooseInitialTickets().size()));
-
-                    playerValue.receiveInfo(nextPlayer.canPlay());
+                    // take the three first of the tickets pile
+                    SortedBag<Ticket> retainedTickets =
+                            playerValue.chooseTickets(
+                                    SortedBag.of(
+                                            tickets.toList()
+                                                    .subList(0, Constants.IN_GAME_TICKETS_COUNT)));
+                    playerValue.receiveInfo(currentPlayer.keptTickets(retainedTickets.size()));
+                    players.forEach((Id, both) -> both.receiveInfo(nextPlayer.canPlay()));
                     break;
 
                 case DRAW_CARDS:
-                    CardState cardState = CardState.of(Deck.of(Constants.ALL_CARDS, new Random()));
                     int totalNumberOfPossibleCardsToDraw = 2;
                     for (int i = 0; i < totalNumberOfPossibleCardsToDraw; i++) {
                         if (playerValue.drawSlot() == -1)
-                            playerValue.receiveInfo(currentPlayer.drewBlindCard());
+                            players.forEach(
+                                    (playerId, allPlayers) ->
+                                            allPlayers.receiveInfo(currentPlayer.drewBlindCard()));
                         else
-                            playerValue.receiveInfo(
-                                    currentPlayer.drewVisibleCard(cardState.topDeckCard()));
+                            players.forEach(
+                                    (playerId, allPlayers) ->
+                                            allPlayers.receiveInfo(
+                                                    currentPlayer.drewVisibleCard(
+                                                            cardState.topDeckCard())));
                     }
-                    playerValue.receiveInfo(nextPlayer.canPlay());
+
+                    players.forEach((Id, both) -> both.receiveInfo(nextPlayer.canPlay()));
                     break;
 
                 case CLAIM_ROUTE:
                     Route claimedRoute = playerValue.claimedRoute();
                     SortedBag<Card> initialClaimCards = playerValue.initialClaimCards();
-                    int drawnCardsSize = playerValue.claimedRoute().length();
-                    playerValue.chooseAdditionalCards(List.of(initialClaimCards));
+                    List<Card> drawnCards = new ArrayList<>();
+
+                    for (int i = 0; i < Constants.ADDITIONAL_TUNNEL_CARDS; i++) {
+                        drawnCards.add(cardState.topDeckCard());
+                        cardState.withoutTopDeckCard();
+                    }
+
+                    SortedBag<Card> additionalCardsToPlay =
+                            playerValue.chooseAdditionalCards(List.of(initialClaimCards));
+                    List<Card> initialAndAdditionalCards =
+                            initialClaimCards.toList().stream()
+                                    .filter(additionalCardsToPlay.toList()::add)
+                                    .collect(Collectors.toList());
 
                     if (claimedRoute.level().equals(Route.Level.OVERGROUND))
-                        playerValue.receiveInfo(
-                                currentPlayer.claimedRoute(claimedRoute, initialClaimCards));
+                        players.forEach(
+                                (playerId, allPlayers) ->
+                                        allPlayers.receiveInfo(
+                                                currentPlayer.claimedRoute(
+                                                        claimedRoute, initialClaimCards)));
                     else
-                        //   for(int i = 0; i < drawnCardsSize; i++){
-                        playerValue.receiveInfo(
-                                currentPlayer.attemptsTunnelClaim(
-                                        claimedRoute,
-                                        initialClaimCards)); // TODO have to use drawnCards
-                    // but where do u get it? &&
-                    // currentPlayer.drewAdditionalCards();
+                        players.forEach(
+                                (playerId, allPlayers) ->
+                                        allPlayers.receiveInfo(
+                                                currentPlayer.attemptsTunnelClaim(
+                                                        claimedRoute,
+                                                        SortedBag.of(initialAndAdditionalCards))));
+                    players.forEach(
+                            (playerId, allPlayers) ->
+                                    allPlayers.receiveInfo(
+                                            currentPlayer.drewAdditionalCards(
+                                                    SortedBag.of(drawnCards),
+                                                    claimedRoute.additionalClaimCardsCount(
+                                                            initialClaimCards,
+                                                            SortedBag.of(drawnCards)))));
 
-                    playerValue.receiveInfo(nextPlayer.canPlay());
+                    if (additionalCardsToPlay.isEmpty())
+                        players.forEach(
+                                (id, both) ->
+                                        both.receiveInfo(
+                                                currentPlayer.didNotClaimRoute(claimedRoute)));
+                    players.forEach((Id, both) -> both.receiveInfo(nextPlayer.canPlay()));
                     break;
             }
+//            PublicPlayerState a = new PublicPlayerState(tickets.size(), c)
         }
     }
 }
