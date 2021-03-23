f6e8497f7afbe24371061821e5a7e050dcf5fcb9
lucamouchel <lucamouchel13@gmail.com>
2021-03-23 09:32:48 +0100

    still WIP but if u wanna read

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -23,8 +23,8 @@ public final class Game {
         players.forEach((playerId, player) -> player.initPlayers(playerId, playerNames));
         PlayerId firstPlayer = PlayerId.ALL.get(rng.nextInt(PlayerId.ALL.size()));
 
-        Info player1 = new Info(firstPlayer.name());
-        Info player2 = new Info(firstPlayer.next().name());
+        Info currentPlayer = new Info(firstPlayer.name());
+        Info nextPlayer = new Info(firstPlayer.next().name());
 
         players.forEach(
                 (playerId, player) ->
@@ -41,41 +41,54 @@ public final class Game {
                                                         - player.chooseInitialTickets().size())));
 
         for (Map.Entry<PlayerId, Player> player : players.entrySet()) {
-            player.getValue().receiveInfo(new Info(player.getKey().name()).canPlay());
+            player.getValue().receiveInfo(currentPlayer.canPlay());
+            Player playerValue = player.getValue();
+
             switch (player.getValue().nextTurn()) {
                 case DRAW_TICKETS:
-                    player.getValue()
-                            .receiveInfo(
-                                    new Info(player.getKey().name())
-                                            .drewTickets(Constants.IN_GAME_TICKETS_COUNT));
-                    player.getValue().chooseTickets(tickets);
-                    player.getValue()
-                            .receiveInfo(
-                                    new Info(player.getKey().name())
-                                            .keptTickets(
-                                                    player.getValue()
-                                                            .chooseInitialTickets()
-                                                            .size()));
+                    playerValue.receiveInfo(
+                            currentPlayer.drewTickets(Constants.IN_GAME_TICKETS_COUNT));
 
+                    playerValue.chooseTickets(tickets);
+                    playerValue.receiveInfo(
+                            currentPlayer.keptTickets(
+                                    player.getValue().chooseInitialTickets().size()));
+
+                    playerValue.receiveInfo(nextPlayer.canPlay());
                     break;
+
                 case DRAW_CARDS:
                     CardState cardState = CardState.of(Deck.of(Constants.ALL_CARDS, new Random()));
                     int totalNumberOfPossibleCardsToDraw = 2;
                     for (int i = 0; i < totalNumberOfPossibleCardsToDraw; i++) {
-                        if (player.getValue().drawSlot() == -1)
-                            player.getValue()
-                                    .receiveInfo(new Info(player.getKey().name()).drewBlindCard());
+                        if (playerValue.drawSlot() == -1)
+                            playerValue.receiveInfo(currentPlayer.drewBlindCard());
                         else
-                            player.getValue()
-                                    .receiveInfo(
-                                            new Info(player.getKey().name())
-                                                    .drewVisibleCard(cardState.topDeckCard()));
+                            playerValue.receiveInfo(
+                                    currentPlayer.drewVisibleCard(cardState.topDeckCard()));
                     }
+                    playerValue.receiveInfo(nextPlayer.canPlay());
+                    break;
+
                 case CLAIM_ROUTE:
-                    player.getValue().claimedRoute();
-                    player.getValue().initialClaimCards();
-                    player.getValue()
-                            .chooseAdditionalCards(List.of(player.getValue().initialClaimCards()));
+                    Route claimedRoute = playerValue.claimedRoute();
+                    SortedBag<Card> initialClaimCards = playerValue.initialClaimCards();
+                    int drawnCardsSize = playerValue.claimedRoute().length();
+                    playerValue.chooseAdditionalCards(List.of(initialClaimCards));
+
+                    if (claimedRoute.level().equals(Route.Level.OVERGROUND))
+                        playerValue.receiveInfo(
+                                currentPlayer.claimedRoute(claimedRoute, initialClaimCards));
+                    else
+                        //   for(int i = 0; i < drawnCardsSize; i++){
+                        playerValue.receiveInfo(
+                                currentPlayer.attemptsTunnelClaim(
+                                        claimedRoute,
+                                        initialClaimCards)); // TODO have to use drawnCards
+                    // but where do u get it? &&
+                    // currentPlayer.drewAdditionalCards();
+
+                    playerValue.receiveInfo(nextPlayer.canPlay());
                     break;
             }
         }
