88b0b79edad96c4c986a6c30416c359c589d537f
Hugues Devimeux <thespourieye@gmail.com>
2021-04-07 00:29:27 +0200

    Added first tests
    Signed-off-by: Hugues Devimeux <thespourieye@gmail.com>

--- src/test/test/ch/epfl/tchu/game/GameTest.java
+++ src/test/test/ch/epfl/tchu/game/GameTest.java
@@ -2,178 +2,187 @@ package ch.epfl.tchu.game;
 
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.test.TestRandomizer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
 
+@PrepareForTest(GameState.class)
 class GameTest {
 
+    @Spy
+    private StandardTestedPlayer mockedPlayer2;
+
+    @Spy
+    private StandardTestedPlayer mockedPlayer1;
+    private Map<PlayerId, String> playersNames;
+
+    @BeforeEach
+    void setUp() {
+        mockedPlayer1 = spy(new StandardTestedPlayer(PlayerId.PLAYER_1, "Alice"));
+        mockedPlayer2 = spy(new StandardTestedPlayer(PlayerId.PLAYER_2, "Bob"));
+        playersNames = Map.of(PlayerId.PLAYER_1, "Alice", PlayerId.PLAYER_2, "Bob");
+    }
+
+
     @Test
-    void testStandardGame() {
-        Map<PlayerId, String> playersNames =
-                Map.of(PlayerId.PLAYER_1, "Alice", PlayerId.PLAYER_2, "Bob");
+    void whenPlayer1OnlyTriesToTakeRoutes() {
+        StandardTestedPlayer player1 =
+                mockedPlayer1.whoIsADummyPlayer().whoAlwaysTriesToTakeARouteWhenPossible();
+        // the route the player wants to claim is asked
         Map<PlayerId, Player> players =
-                PlayerId.ALL.stream()
-                        .collect(
-                                Collectors.toMap(
-                                        playerId -> playerId, StandardTestedPlayer::new));
-        Player mockedPlayer = mock(StandardTestedPlayer.class);
+                Map.of(
+                        PlayerId.PLAYER_1,
+                        player1,
+                        PlayerId.PLAYER_2,
+                        mockedPlayer2.whoIsADummyPlayer());
+        Game.play(players, playersNames, SortedBag.of(ChMap.tickets()), TestRandomizer.newRandom());
+
+        // NOTE : This fails because when player_1 is saying he wants to take a route, player2 is
+        // asked to get which route.
+    }
+
+    @Test
+    void whenTwoPlayersTryBothToTakeRoutes() {
+        Map<PlayerId, Player> players =
+                Map.of(
+                        PlayerId.PLAYER_1,
+                        mockedPlayer1.whoIsADummyPlayer().whoAlwaysTriesToTakeARouteWhenPossible(),
+                        PlayerId.PLAYER_2,
+                        mockedPlayer2.whoIsADummyPlayer().whoAlwaysTriesToTakeARouteWhenPossible());
+
         Game.play(players, playersNames, SortedBag.of(ChMap.tickets()), TestRandomizer.newRandom());
     }
+
 
     // Players utils for tests.
 
     private static class StandardTestedPlayer implements Player {
 
-        private final int MAX_TURNS = 200;
-
+        public final String name;
         private final PlayerId ownId;
-
-        private final int turnCounter = 0;
+        private SortedBag<Ticket> initialTickets;
         private PublicGameState gameState;
         private PlayerState playerState;
-        private SortedBag<Ticket> intitialTickets;
 
-        public StandardTestedPlayer(PlayerId ownId) {
+        private Route nextRouteToClaim;
+        private SortedBag<Card> nextInitialCardsUsedToClaimRoute;
 
+        public StandardTestedPlayer(PlayerId ownId, String name) {
             this.ownId = ownId;
+            this.name = name;
         }
 
-        /**
-         * Called upon when starting the game to communicate the ID of the players as well as the
-         * name of all the players, including his or hers (in <code>playernames</code>).
-         *
-         * @param ownId       the players' id
-         * @param playerNames all the names of all the players
-         */
+        public StandardTestedPlayer whoAlwaysTriesToTakeARouteWhenPossible() {
+            doAnswer(
+                    invocationOnMock -> {
+                        List<Route> claimableRoutes =
+                                ChMap.routes().stream()
+                                        .filter(this.playerState::canClaimRoute)
+                                        .collect(Collectors.toList());
+                        if (claimableRoutes.size() == 0) {
+                            return TurnKind.DRAW_CARDS;
+                        }
+                        this.nextRouteToClaim = claimableRoutes.get(0);
+                        this.nextInitialCardsUsedToClaimRoute =
+                                this.playerState
+                                        .possibleClaimCards(nextRouteToClaim)
+                                        .get(0);
+                        return TurnKind.CLAIM_ROUTE;
+                    })
+                    .when(this)
+                    .nextTurn();
+
+            doAnswer(o -> this.nextRouteToClaim).when(this).claimedRoute();
+            doAnswer(o -> this.nextInitialCardsUsedToClaimRoute).when(this).initialClaimCards();
+            return this;
+        }
+
+        public StandardTestedPlayer whoIsADummyPlayer() {
+            // Outputs the infos.
+            doAnswer(
+                    invocationOnMock -> {
+                        System.out.printf("LOGGING from : %s%n", this.name);
+                        System.out.println(invocationOnMock.<String>getArgument(0));
+                        return null;
+                    })
+                    .when(this)
+                    .receiveInfo(anyString());
+            // Returns the first ticket.
+            doAnswer(invocationOnMock -> invocationOnMock.<SortedBag<Ticket>>getArgument(0).get(0))
+                    .when(this)
+                    .chooseTickets(any(SortedBag.class));
+            // Always select to draw a new card
+            doAnswer(o -> TurnKind.DRAW_CARDS).when(this).nextTurn();
+            // Always draw blindly.
+            doReturn(-1).when(this).drawSlot();
+            return this;
+        }
+
         @Override
         public void initPlayers(PlayerId ownId, Map<PlayerId, String> playerNames) {
-            assertEquals(this.ownId, ownId);
         }
 
-        /**
-         * Called when an information has to be transmitted to a player under the form of a String.
-         *
-         * @param info information to be transmitted
-         */
         @Override
         public void receiveInfo(String info) {
-            System.out.println(info);
-            // Meant to be mocked.
         }
 
-        /**
-         * Called when the game state changes - to inform the player of the <code>newState</code> as
-         * well as his/her own state.
-         *
-         * @param newState new state of the game
-         * @param ownState the state of the player
-         */
         @Override
         public void updateState(PublicGameState newState, PlayerState ownState) {
-
-            this.gameState = newState;
             this.playerState = ownState;
+            this.gameState = newState;
         }
 
-        /**
-         * Called at the BEGINNING of the game to communicate the 5 tickets that have been
-         * distributed.
-         *
-         * @param tickets 5 tickets distributed
-         */
         @Override
         public void setInitialTicketChoice(SortedBag<Ticket> tickets) {
-            assertEquals(5, tickets.size());
-            this.intitialTickets = tickets;
-            // Meant to be mocked. Called once!
+            this.initialTickets = tickets;
         }
 
-        /**
-         * Called at the beginning of the game to ask the player which tickets the player wishes to
-         * keep (via <code>setInitialTicketChoice</code>).
-         *
-         * @return the bag of tickets the player wants to keep
-         */
         @Override
         public SortedBag<Ticket> chooseInitialTickets() {
-            return SortedBag.of(this.intitialTickets.get(0));
+            return SortedBag.of(this.initialTickets.get(0));
         }
 
-        /**
-         * Called at the beginning of each round, to know what action the player decides to take.
-         *
-         * @return the player's chosen course of action at the beginning of the round
-         */
         @Override
         public TurnKind nextTurn() {
+            fail("This Method should not be called here");
             return null;
         }
 
-        /**
-         * Called when the player decides to draw additional tickets during the game, as to
-         * communication the drawn tickets and to know which ones the player keeps.
-         *
-         * @param options bag of tickets the player can choose from
-         * @return communication of drawn tickets to know which ones are kept
-         */
         @Override
         public SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options) {
+            fail("This Method should not be called here");
             return null;
         }
 
-        /**
-         * Called when the player has decided to draw cards (can be locomotives) to know where he
-         * wants to to draw them from. It can be a faceUp card - to which the method returns int in
-         * [0,4]. It can be a deck card - to which the method returns <code>Constants.DECK_SLOT
-         * </code> aka -1.
-         *
-         * @return an int that depends on what type of card is drawn
-         */
         @Override
         public int drawSlot() {
+            fail("This Method should not be called here");
             return 0;
         }
 
-        /**
-         * Called when the player has decided (attempted to) take over a route - to know which route
-         * it is.
-         *
-         * @return the route the player has tried to take over
-         */
         @Override
         public Route claimedRoute() {
+            fail("This Method should not be called here");
             return null;
         }
 
-        /**
-         * Called when the player has decided to (or attempted to) take over a route - to know which
-         * card(s) they want to use initially to do so.
-         *
-         * @return the initial cards the player wants to use to take over a route
-         */
         @Override
         public SortedBag<Card> initialClaimCards() {
+            fail("This Method should not be called here");
             return null;
         }
 
-        /**
-         * Called when the player has decided to attempt to take a tunnel (UNDERGROUND route) and
-         * that additional cards have to be used - to ultimately know which cards the player wants
-         * to use. The available cards he can use is the parameter options. If the returned
-         * SortedBag is empty, it means the player doesn't want (or can't) pick any options.
-         *
-         * @param options list of cards with which the player can choose to take over a tunnel
-         * @return the additional cards the player wants to use to take over a tunnel, or an empty
-         * sortedBag if he can't or does not want to
-         */
         @Override
         public SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options) {
+            fail("This Method should not be called here");
             return null;
         }
     }
