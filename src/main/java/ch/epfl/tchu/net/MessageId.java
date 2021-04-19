8d7af693a1d5e7097516f6540dc6313d9e161bad
luca mouchel <73081373+lucamouchel@users.noreply.github.com>
2021-04-19 21:44:16 +0200

    etape 7 - 3.1 MessageId.java (#51)
    * etape 7 - 3.1 MessageId.java
    
    * format
    
    * Update src/main/java/ch/epfl/tchu/net/MessageId.java
    
    Co-authored-by: github-actions <>
    Co-authored-by: Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>

---
+++ src/main/java/ch/epfl/tchu/net/MessageId.java
@@ -0,0 +1,21 @@
+package ch.epfl.tchu.net;
+
+/**
+ * Enumeration of all the possible messages sent to the players.
+ *
+ * @author Hugues Devimeux (327282)
+ * @author Luca Mouchel (324748)
+ */
+public enum MessageId {
+    INIT_PLAYERS,
+    RECEIVE_INFO,
+    UPDATE_STATE,
+    SET_INITIAL_TICKETS,
+    CHOOSE_INITIAL_TICKETS,
+    NEXT_TURN,
+    CHOOSE_TICKETS,
+    DRAW_SLOT,
+    ROUTE,
+    CARDS,
+    CHOOSE_ADDITIONAL_CARDS
+}
