9937b5105c0af129ef5ff004076fdbf30f43daa5
Hugues Devimeux <36239975+huguesdevimeux@users.noreply.github.com>
2021-05-12 11:17:50 +0200

    WIP 10 3.2 graphical player 1 (#71)
    * First glance of GraphicalPlayer.java
    
    * Fixed ObservableGameState.java
    
    * Added GraphicalPlayer.java
    
    * Added title bitch
    
    Signed-off-by: Hugues Devimeux <thespourieye@gmail.com>
    
    * Added asserts bitch
    
    Signed-off-by: Hugues Devimeux <thespourieye@gmail.com>
    
    * Added emptyHandler logic
    
    Signed-off-by: Hugues Devimeux <thespourieye@gmail.com>
    
    * Added stuff blabla read the code bitch
    
    Signed-off-by: Hugues Devimeux <thespourieye@gmail.com>
    
    * Added string converter
    
    Signed-off-by: Hugues Devimeux <thespourieye@gmail.com>
    
    * Added assert stuff
    
    Signed-off-by: Hugues Devimeux <thespourieye@gmail.com>
    
    * only 4 messages appear
    
    * Moved CardBagStringConverter into private
    
    * copy of playerNames
    
    * New constant
    
    * replaced and used constants
    
    * Added dynamic plural for popup
    
    * Fixed variable name
    
    * fixes
    
    * fixes
    
    * removed redundant this
    
    * fix
    
    * Important FIX
    
    * fixed cards %
    
    Co-authored-by: lucamouchel <lucamouchel13@gmail.com>

--- src/main/java/ch/epfl/tchu/gui/ObservableGameState.java
+++ src/main/java/ch/epfl/tchu/gui/ObservableGameState.java
@@ -10,8 +10,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static ch.epfl.tchu.game.Constants.FACE_UP_CARDS_COUNT;
-import static ch.epfl.tchu.game.Constants.FACE_UP_CARD_SLOTS;
+import static ch.epfl.tchu.game.Constants.*;
 
 /**
  * Representation of the observable state/part of the game.
@@ -73,9 +72,8 @@ public class ObservableGameState {
         percentageOfTicketsRemaining.set(
                 (int) ((1 - (double) numOfTicketsUsed / ChMap.tickets().size()) * 100));
 
-        double cardQuotient =
-                (double) newGameState.cardState().deckSize() / Constants.ALL_CARDS.size();
-        percentageOfCardsRemaining.set((int) ((1 - cardQuotient) * 100));
+        percentageOfCardsRemaining.set(
+                (int) ((newGameState.cardState().deckSize() / (double) TOTAL_CARDS_COUNT) * 100));
 
         // setting the face up cards
         for (int slot : FACE_UP_CARD_SLOTS) {
@@ -134,6 +132,7 @@ public class ObservableGameState {
             }
         }
     }
+
     /**
      * Boolean method to evaluate if the {@code route} has a neighbour.
      *
@@ -182,8 +181,10 @@ public class ObservableGameState {
     }
 
     // simple getters as Read Only properties
+
     /**
      * Returns the percentage of tickets remaining.
+     *
      * @return percentage of tickets remaining
      */
     public ReadOnlyIntegerProperty percentageTickets() {
@@ -192,6 +193,7 @@ public class ObservableGameState {
 
     /**
      * Returns the percentage of cards remaining.
+     *
      * @return percentage of cards remaining
      */
     public ReadOnlyIntegerProperty percentageCards() {
@@ -200,6 +202,7 @@ public class ObservableGameState {
 
     /**
      * Returns the face up card at index slot.
+     *
      * @param slot index of the face up card
      * @return face up card at index slot
      */
@@ -209,6 +212,7 @@ public class ObservableGameState {
 
     /**
      * Returns the owner of the route. (either PLAYER_1 or 2 or null if nobody "owns" it)
+     *
      * @param route route's owner
      * @return the route's owner, or null if it does not have any
      */
@@ -218,6 +222,7 @@ public class ObservableGameState {
 
     /**
      * Returns the player's ticket count.
+     *
      * @param playerId specified player
      * @return the specified player's ticket count
      */
@@ -227,6 +232,7 @@ public class ObservableGameState {
 
     /**
      * Returns the player's card count.
+     *
      * @param playerId specified player
      * @return the specified player's card count
      */
@@ -236,6 +242,7 @@ public class ObservableGameState {
 
     /**
      * Returns the player's car count.
+     *
      * @param playerId specified player
      * @return the specified player's car count
      */
@@ -245,6 +252,7 @@ public class ObservableGameState {
 
     /**
      * Returns the player's claim points.
+     *
      * @param playerId specified player
      * @return the specified player's claim points
      */
@@ -254,6 +262,7 @@ public class ObservableGameState {
 
     /**
      * Returns the current player's list of tickets.
+     *
      * @return the current player's list of tickets
      */
     public ObservableList<Ticket> playersTicketsList() {
@@ -262,6 +271,7 @@ public class ObservableGameState {
 
     /**
      * Returns the current player's amount of cards of type {@code card}.
+     *
      * @param card the card to evaluate it's total amount in the player's cards
      * @return the amount of cards of type {@code card}
      */
@@ -271,6 +281,7 @@ public class ObservableGameState {
 
     /**
      * Returns whether the player can claim the route.
+     *
      * @param route to evaluate whether the player can claim it
      * @return true if the player can claim the route, else false
      */
@@ -278,8 +289,10 @@ public class ObservableGameState {
         return playerCanClaimRoute.get(ChMap.routes().indexOf(route));
     }
 
+
     /**
      * Returns a boolean from a method in {@code PublicGameState}.
+     *
      * @return true if the player can draw tickets, else false
      */
     public ReadOnlyBooleanProperty canDrawTickets() {
@@ -288,6 +301,7 @@ public class ObservableGameState {
 
     /**
      * Returns a boolean from a method in {@code PublicGameState}.
+     *
      * @return true whether the player can draw cards, else false
      */
     public ReadOnlyBooleanProperty canDrawCards() {
@@ -296,6 +310,7 @@ public class ObservableGameState {
 
     /**
      * Returns the player's possible claim cards for the given route.
+     *
      * @param route to extract the possible claim cards from
      * @return the possible claim cards to claim the route
      */
