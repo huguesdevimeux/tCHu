2f09fcd7ef2fe73b743447db04a0c13f091947e7
lucamouchel <lucamouchel13@gmail.com>
2021-05-22 12:50:12 +0200

    just formatting

--- src/main/java/ch/epfl/tchu/gui/DecksViewCreator.java
+++ src/main/java/ch/epfl/tchu/gui/DecksViewCreator.java
@@ -24,123 +24,127 @@ import static ch.epfl.tchu.gui.GuiConstants.*;
  * @author Luca Mouchel (324748)
  */
 final class DecksViewCreator {
-  private static ListView<Ticket> ticketsListView;
+    private static ListView<Ticket> ticketsListView;
 
-  // Not instantiable.
-  private DecksViewCreator() {}
+    // Not instantiable.
+    private DecksViewCreator() {}
 
-  public static Node createCardsView(
-      ObservableGameState observableGameState,
-      ObjectProperty<ActionHandlers.DrawTicketsHandler> drawTicketsHandler,
-      ObjectProperty<ActionHandlers.DrawCardHandler> drawCardHandler) {
+    public static Node createCardsView(
+            ObservableGameState observableGameState,
+            ObjectProperty<ActionHandlers.DrawTicketsHandler> drawTicketsHandler,
+            ObjectProperty<ActionHandlers.DrawCardHandler> drawCardHandler) {
 
-    // Tickets pile.
-    // Button group
-    Button ticketsPile =
-        itemPileWithGauge(StringsFr.TICKETS, observableGameState.percentageTickets());
-    ticketsPile.disableProperty().bind(drawTicketsHandler.isNull());
-    ticketsPile.setOnAction(event -> drawTicketsHandler.get().onDrawTickets());
-    VBox cardsView = new VBox(ticketsPile);
-    cardsView.setId(ID_CARD_PANE);
-    cardsView.getStylesheets().addAll(DECKS_CSS, COLORS_CSS);
+        // Tickets pile.
+        // Button group
+        Button ticketsPile =
+                itemPileWithGauge(StringsFr.TICKETS, observableGameState.percentageTickets());
+        ticketsPile.disableProperty().bind(drawTicketsHandler.isNull());
+        ticketsPile.setOnAction(event -> drawTicketsHandler.get().onDrawTickets());
+        VBox cardsView = new VBox(ticketsPile);
+        cardsView.setId(ID_CARD_PANE);
+        cardsView.getStylesheets().addAll(DECKS_CSS, COLORS_CSS);
 
-    for (int slot : Constants.FACE_UP_CARD_SLOTS) {
-      StackPane displayedCard = individualCard();
-      cardsView.getChildren().add(displayedCard);
-      displayedCard.disableProperty().bind(drawCardHandler.isNull());
-      displayedCard.setOnMouseClicked(event -> drawCardHandler.get().onDrawCard(slot));
+        for (int slot : Constants.FACE_UP_CARD_SLOTS) {
+            StackPane displayedCard = individualCard();
+            cardsView.getChildren().add(displayedCard);
+            displayedCard.disableProperty().bind(drawCardHandler.isNull());
+            displayedCard.setOnMouseClicked(event -> drawCardHandler.get().onDrawCard(slot));
 
-      observableGameState
-          .faceUpCard(slot)
-          .addListener(
-              (observable, oldValue, newValue) -> {
-                String newColor =
-                    newValue.color() == null ? STYLE_CLASS_COLOR_NEUTRAL : newValue.color().name();
-                // Remove any Color css attribute and replace by the new color.
-                displayedCard
-                    .getStyleClass()
-                    .removeIf(
-                        (s) ->
-                            STYLE_CLASSES_COLOR.contains(s) || s.equals(STYLE_CLASS_COLOR_NEUTRAL));
-                displayedCard.getStyleClass().add(newColor);
-              });
-    }
-    Button cardsPile = itemPileWithGauge(StringsFr.CARDS, observableGameState.percentageCards());
-    cardsPile.disableProperty().bind(drawCardHandler.isNull());
-    cardsPile.setOnAction(e -> drawCardHandler.get().onDrawCard(Constants.DECK_SLOT));
-    cardsView.getChildren().add(cardsPile);
-    return cardsView;
-  }
-
-  public static Node createHandView(ObservableGameState observableGameState) {
-    // TICKETS HAND VIEW
-    ticketsListView = new ListView<>();
-    ticketsListView.setItems(observableGameState.playersTicketsList());
-    ticketsListView.getStylesheets().add(MAP_CSS);
-    ticketsListView.setId(ID_TICKETS);
-
-    HBox cardsHandPanel = new HBox();
-    cardsHandPanel.setId(ID_HAND_PANE);
-
-    for (Card card : Card.ALL) {
-      StackPane cardOfHand = individualCard();
-      String color = card.color() == null ? STYLE_CLASS_COLOR_NEUTRAL : card.color().name();
-      cardOfHand.getStyleClass().addAll(color);
-      cardOfHand
-          .visibleProperty()
-          .bind(observableGameState.playersNumberOfCards(card).greaterThan(0));
-
-      // Count.
-      Text count = new Text();
-      count.textProperty().bind(observableGameState.playersNumberOfCards(card).asString());
-      count
-          .visibleProperty()
-          .bind(
-              observableGameState
-                  .playersNumberOfCards(card)
-                  .greaterThan(MIN_CARDS_NUMBER_DISPLAYED));
-      count.getStyleClass().add(STYLE_CLASS_COUNT);
-      cardOfHand.getChildren().add(count);
-      cardsHandPanel.getChildren().add(cardOfHand);
+            observableGameState
+                    .faceUpCard(slot)
+                    .addListener(
+                            (observable, oldValue, newValue) -> {
+                                String newColor =
+                                        newValue.color() == null
+                                                ? STYLE_CLASS_COLOR_NEUTRAL
+                                                : newValue.color().name();
+                                // Remove any Color css attribute and replace by the new color.
+                                displayedCard
+                                        .getStyleClass()
+                                        .removeIf(
+                                                (s) ->
+                                                        STYLE_CLASSES_COLOR.contains(s)
+                                                                || s.equals(
+                                                                        STYLE_CLASS_COLOR_NEUTRAL));
+                                displayedCard.getStyleClass().add(newColor);
+                            });
+        }
+        Button cardsPile =
+                itemPileWithGauge(StringsFr.CARDS, observableGameState.percentageCards());
+        cardsPile.disableProperty().bind(drawCardHandler.isNull());
+        cardsPile.setOnAction(e -> drawCardHandler.get().onDrawCard(Constants.DECK_SLOT));
+        cardsView.getChildren().add(cardsPile);
+        return cardsView;
     }
 
-    HBox handView = new HBox(ticketsListView, cardsHandPanel);
-    handView.getStylesheets().addAll(DECKS_CSS, COLORS_CSS);
-    return handView;
-  }
+    public static Node createHandView(ObservableGameState observableGameState) {
+        // TICKETS HAND VIEW
+        ticketsListView = new ListView<>();
+        ticketsListView.setItems(observableGameState.playersTicketsList());
+        ticketsListView.getStylesheets().add(MAP_CSS);
+        ticketsListView.setId(ID_TICKETS);
 
-  public static ListView<Ticket> getTicketsListView() {
-    return ticketsListView;
-  }
+        HBox cardsHandPanel = new HBox();
+        cardsHandPanel.setId(ID_HAND_PANE);
 
-  private static StackPane individualCard() {
-    // Inner icon of cards. Sorted in an exterior fashion.
-    Rectangle inner1 = new Rectangle(60, 90);
-    inner1.getStyleClass().add(STYLE_CLASS_OUTSIDE);
-    Rectangle inner2 = new Rectangle(40, 70);
-    inner2.getStyleClass().addAll(STYLE_CLASS_FILLED, STYLE_CLASS_INSIDE);
-    Rectangle inner3 = new Rectangle(40, 70);
-    inner3.getStyleClass().add(STYLE_CLASS_TRAIN_IMAGE);
+        for (Card card : Card.ALL) {
+            StackPane cardOfHand = individualCard();
+            String color = card.color() == null ? STYLE_CLASS_COLOR_NEUTRAL : card.color().name();
+            cardOfHand.getStyleClass().addAll(color);
+            cardOfHand
+                    .visibleProperty()
+                    .bind(observableGameState.playersNumberOfCards(card).greaterThan(0));
 
-    // Outer layout.
-    StackPane cardOfHand = new StackPane();
-    cardOfHand.getChildren().addAll(inner1, inner2, inner3);
-    cardOfHand.getStyleClass().add(STYLE_CLASS_CARD);
-    return cardOfHand;
-  }
+            // Count.
+            Text count = new Text();
+            count.textProperty().bind(observableGameState.playersNumberOfCards(card).asString());
+            count.visibleProperty()
+                    .bind(
+                            observableGameState
+                                    .playersNumberOfCards(card)
+                                    .greaterThan(MIN_CARDS_NUMBER_DISPLAYED));
+            count.getStyleClass().add(STYLE_CLASS_COUNT);
+            cardOfHand.getChildren().add(count);
+            cardsHandPanel.getChildren().add(cardOfHand);
+        }
 
-  private static Button itemPileWithGauge(
-      String itemName, ReadOnlyIntegerProperty percentageProperty) {
-    Button itemPile = new Button(itemName);
-    itemPile.getStyleClass().add(STYLE_CLASS_GAUGED);
+        HBox handView = new HBox(ticketsListView, cardsHandPanel);
+        handView.getStylesheets().addAll(DECKS_CSS, COLORS_CSS);
+        return handView;
+    }
 
-    Rectangle backgroundButtonGraphic = new Rectangle(50, 5);
-    backgroundButtonGraphic.getStyleClass().add(STYLE_CLASS_BACKGROUND);
-    Rectangle foregroundButtonGraphic = new Rectangle(50, 5);
-    foregroundButtonGraphic.getStyleClass().add(STYLE_CLASS_FOREGROUND);
-    foregroundButtonGraphic.widthProperty().bind(percentageProperty.divide(2));
+    public static ListView<Ticket> getTicketsListView() {
+        return ticketsListView;
+    }
 
-    itemPile.setGraphic(new Group(backgroundButtonGraphic, foregroundButtonGraphic));
-    return itemPile;
-  }
+    private static StackPane individualCard() {
+        // Inner icon of cards. Sorted in an exterior fashion.
+        Rectangle inner1 = new Rectangle(60, 90);
+        inner1.getStyleClass().add(STYLE_CLASS_OUTSIDE);
+        Rectangle inner2 = new Rectangle(40, 70);
+        inner2.getStyleClass().addAll(STYLE_CLASS_FILLED, STYLE_CLASS_INSIDE);
+        Rectangle inner3 = new Rectangle(40, 70);
+        inner3.getStyleClass().add(STYLE_CLASS_TRAIN_IMAGE);
+
+        // Outer layout.
+        StackPane cardOfHand = new StackPane();
+        cardOfHand.getChildren().addAll(inner1, inner2, inner3);
+        cardOfHand.getStyleClass().add(STYLE_CLASS_CARD);
+        return cardOfHand;
+    }
+
+    private static Button itemPileWithGauge(
+            String itemName, ReadOnlyIntegerProperty percentageProperty) {
+        Button itemPile = new Button(itemName);
+        itemPile.getStyleClass().add(STYLE_CLASS_GAUGED);
+
+        Rectangle backgroundButtonGraphic = new Rectangle(50, 5);
+        backgroundButtonGraphic.getStyleClass().add(STYLE_CLASS_BACKGROUND);
+        Rectangle foregroundButtonGraphic = new Rectangle(50, 5);
+        foregroundButtonGraphic.getStyleClass().add(STYLE_CLASS_FOREGROUND);
+        foregroundButtonGraphic.widthProperty().bind(percentageProperty.divide(2));
+
+        itemPile.setGraphic(new Group(backgroundButtonGraphic, foregroundButtonGraphic));
+        return itemPile;
+    }
 }
--- src/main/java/ch/epfl/tchu/gui/MapViewCreator.java
+++ src/main/java/ch/epfl/tchu/gui/MapViewCreator.java
@@ -1,7 +1,9 @@
 package ch.epfl.tchu.gui;
 
 import ch.epfl.tchu.SortedBag;
-import ch.epfl.tchu.game.*;
+import ch.epfl.tchu.game.Card;
+import ch.epfl.tchu.game.ChMap;
+import ch.epfl.tchu.game.Route;
 import ch.epfl.tchu.gui.ActionHandlers.ChooseCardsHandler;
 import ch.epfl.tchu.gui.ActionHandlers.ClaimRouteHandler;
 import javafx.beans.property.ObjectProperty;
@@ -14,7 +16,6 @@ import javafx.scene.shape.Rectangle;
 
 import java.util.List;
 
-import static ch.epfl.tchu.game.ChMap.stations;
 import static ch.epfl.tchu.gui.GuiConstants.*;
 
 /**
@@ -24,104 +25,109 @@ import static ch.epfl.tchu.gui.GuiConstants.*;
  * @author Hugues Devimeux (327282)
  */
 final class MapViewCreator {
-  private static Pane gameMapPane;
-  /** Not instantiable. */
-  private MapViewCreator() {}
+    private static Pane gameMapPane;
+    /** Not instantiable. */
+    private MapViewCreator() {}
 
-  /**
-   * Method in charge of creating the whole view of the map. ie the routes, the map, the colors.
-   *
-   * @param obsGameState the observable part of the game.
-   * @param routeHandler responsible for an attempt to claim a route.
-   * @param cardChooser responsible for choosing cards.
-   * @return the node that contains all elements in the mapView - an instance of Pane in this case.
-   */
-  public static Node createMapView(
-      ObservableGameState obsGameState,
-      ObjectProperty<ClaimRouteHandler> routeHandler,
-      CardChooser cardChooser) {
-    gameMapPane = new Pane();
-    gameMapPane.getStylesheets().addAll(MAP_CSS, COLORS_CSS);
-    gameMapPane.getChildren().add(new ImageView());
-
-    for (Route route : ChMap.routes()) {
-      Group mainRouteGroup = new Group();
-      mainRouteGroup.setId(route.id());
-      mainRouteGroup
-          .getStyleClass()
-          .addAll(
-              STYLE_CLASS_ROUTE,
-              route.level().name(),
-              route.color() == null ? STYLE_CLASS_COLOR_NEUTRAL : route.color().name());
-
-      for (int i = 1; i <= route.length(); i++) {
-        Group eachRoutesBlock = new Group();
-        eachRoutesBlock.setId(String.format("%s_%s", route.id(), i));
-
-        Rectangle rectForTracks = new Rectangle(36, 12);
-        rectForTracks.getStyleClass().addAll(STYLE_CLASS_TRACK, STYLE_CLASS_FILLED);
-        eachRoutesBlock.getChildren().add(rectForTracks);
-
-        Group routesCars = new Group();
-        routesCars.getStyleClass().add(STYLE_CLASS_CAR);
-
-        Rectangle rectForCars = new Rectangle(36, 12);
-        rectForCars.getStyleClass().add(STYLE_CLASS_FILLED);
-        Circle circle1 = new Circle(12, 6, 3);
-        Circle circle2 = new Circle(24, 6, 3);
-        routesCars.getChildren().addAll(rectForCars, circle1, circle2);
-
-        // established hierarchy : cars group -> block group -> route group
-        eachRoutesBlock.getChildren().add(routesCars);
-        mainRouteGroup.getChildren().addAll(eachRoutesBlock);
-      }
-
-      gameMapPane.getChildren().add(mainRouteGroup);
-      // If the route handler is null or the player can't claim the route
-      // we disable the player's attempt to claim the route, ie pressing
-      // on a route will do nothing.
-      mainRouteGroup
-          .disableProperty()
-          .bind(routeHandler.isNull().or(obsGameState.playerCanClaimRoute(route).not()));
-
-      // If the route is owned by a player, we fill the route's blocks
-      // with the corresponding player's color (light blue for PLAYER_1 for ex)
-      obsGameState
-          .getRoutesOwner(route)
-          .addListener(
-              (observableValue, oldValue, newValue) ->
-                  mainRouteGroup.getStyleClass().add(newValue.name()));
-
-      mainRouteGroup.setOnMouseClicked(
-          event -> {
-            List<SortedBag<Card>> possibleClaimCards = obsGameState.possibleClaimCards(route).get();
-            if (possibleClaimCards.size() == 1)
-              routeHandler.get().onClaimRoute(route, possibleClaimCards.get(0));
-            else {
-              ChooseCardsHandler chooseCardsH =
-                  chosenCards -> routeHandler.get().onClaimRoute(route, chosenCards);
-              cardChooser.chooseCards(possibleClaimCards, chooseCardsH);
-            }
-          });
-    }
-    return gameMapPane;
-  }
-
-  public static Pane getGameMapPane() {
-    return gameMapPane;
-  }
-  /**
-   * Interface containing a method intended to be called when the player must choose the cards he
-   * wishes to use to seize a route.
-   */
-  @FunctionalInterface
-  public interface CardChooser {
     /**
-     * Called so user chooses cards he wants to use to claim a route.
+     * Method in charge of creating the whole view of the map. ie the routes, the map, the colors.
      *
-     * @param options cards to choose from
-     * @param handler used when the player has made the choice
+     * @param obsGameState the observable part of the game.
+     * @param routeHandler responsible for an attempt to claim a route.
+     * @param cardChooser responsible for choosing cards.
+     * @return the node that contains all elements in the mapView - an instance of Pane in this
+     *     case.
      */
-    void chooseCards(List<SortedBag<Card>> options, ChooseCardsHandler handler);
-  }
+    public static Node createMapView(
+            ObservableGameState obsGameState,
+            ObjectProperty<ClaimRouteHandler> routeHandler,
+            CardChooser cardChooser) {
+        gameMapPane = new Pane();
+        gameMapPane.getStylesheets().addAll(MAP_CSS, COLORS_CSS);
+        gameMapPane.getChildren().add(new ImageView());
+
+        for (Route route : ChMap.routes()) {
+            Group mainRouteGroup = new Group();
+            mainRouteGroup.setId(route.id());
+            mainRouteGroup
+                    .getStyleClass()
+                    .addAll(
+                            STYLE_CLASS_ROUTE,
+                            route.level().name(),
+                            route.color() == null
+                                    ? STYLE_CLASS_COLOR_NEUTRAL
+                                    : route.color().name());
+
+            for (int i = 1; i <= route.length(); i++) {
+                Group eachRoutesBlock = new Group();
+                eachRoutesBlock.setId(String.format("%s_%s", route.id(), i));
+
+                Rectangle rectForTracks = new Rectangle(36, 12);
+                rectForTracks.getStyleClass().addAll(STYLE_CLASS_TRACK, STYLE_CLASS_FILLED);
+                eachRoutesBlock.getChildren().add(rectForTracks);
+
+                Group routesCars = new Group();
+                routesCars.getStyleClass().add(STYLE_CLASS_CAR);
+
+                Rectangle rectForCars = new Rectangle(36, 12);
+                rectForCars.getStyleClass().add(STYLE_CLASS_FILLED);
+                Circle circle1 = new Circle(12, 6, 3);
+                Circle circle2 = new Circle(24, 6, 3);
+                routesCars.getChildren().addAll(rectForCars, circle1, circle2);
+
+                // established hierarchy : cars group -> block group -> route group
+                eachRoutesBlock.getChildren().add(routesCars);
+                mainRouteGroup.getChildren().addAll(eachRoutesBlock);
+            }
+
+            gameMapPane.getChildren().add(mainRouteGroup);
+            // If the route handler is null or the player can't claim the route
+            // we disable the player's attempt to claim the route, ie pressing
+            // on a route will do nothing.
+            mainRouteGroup
+                    .disableProperty()
+                    .bind(routeHandler.isNull().or(obsGameState.playerCanClaimRoute(route).not()));
+
+            // If the route is owned by a player, we fill the route's blocks
+            // with the corresponding player's color (light blue for PLAYER_1 for ex)
+            obsGameState
+                    .getRoutesOwner(route)
+                    .addListener(
+                            (observableValue, oldValue, newValue) ->
+                                    mainRouteGroup.getStyleClass().add(newValue.name()));
+
+            mainRouteGroup.setOnMouseClicked(
+                    event -> {
+                        List<SortedBag<Card>> possibleClaimCards =
+                                obsGameState.possibleClaimCards(route).get();
+                        if (possibleClaimCards.size() == 1)
+                            routeHandler.get().onClaimRoute(route, possibleClaimCards.get(0));
+                        else {
+                            ChooseCardsHandler chooseCardsH =
+                                    chosenCards ->
+                                            routeHandler.get().onClaimRoute(route, chosenCards);
+                            cardChooser.chooseCards(possibleClaimCards, chooseCardsH);
+                        }
+                    });
+        }
+        return gameMapPane;
+    }
+
+    public static Pane getGameMapPane() {
+        return gameMapPane;
+    }
+    /**
+     * Interface containing a method intended to be called when the player must choose the cards he
+     * wishes to use to seize a route.
+     */
+    @FunctionalInterface
+    public interface CardChooser {
+        /**
+         * Called so user chooses cards he wants to use to claim a route.
+         *
+         * @param options cards to choose from
+         * @param handler used when the player has made the choice
+         */
+        void chooseCards(List<SortedBag<Card>> options, ChooseCardsHandler handler);
+    }
 }
