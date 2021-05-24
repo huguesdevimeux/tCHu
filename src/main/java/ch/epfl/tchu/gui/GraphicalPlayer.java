55593e456cda4fcb7bb739fad84f2a5e20a156ff
Hugues Devimeux <thespourieye@gmail.com>
2021-05-24 14:53:53 +0200

    Removed useless precondition

--- src/main/java/ch/epfl/tchu/gui/GraphicalPlayer.java
+++ src/main/java/ch/epfl/tchu/gui/GraphicalPlayer.java
@@ -126,7 +126,6 @@ public class GraphicalPlayer {
             SortedBag<Ticket> choosableTickets,
             ActionHandlers.ChooseTicketsHandler chooseTicketsHandler) {
         assert isFxApplicationThread();
-        Preconditions.checkArgument(choosableTickets.size() == 3 || choosableTickets.size() == 5);
         int minTickets = choosableTickets.size() - Constants.DISCARDABLE_TICKETS_COUNT;
         String title =
                 String.format(StringsFr.CHOOSE_TICKETS, minTickets, StringsFr.plural(minTickets));
