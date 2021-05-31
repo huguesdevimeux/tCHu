e01ad28e5dc5ad013a5150c76baffabee23ed230
lucamouchel <lucamouchel13@gmail.com>
2021-05-31 10:44:13 +0200

    facilitating writing epfl Ip addresses

--- src/main/java/ch/epfl/tchu/net/MainMenuClientController.java
+++ src/main/java/ch/epfl/tchu/net/MainMenuClientController.java
@@ -4,39 +4,39 @@ import ch.epfl.tchu.gui.GraphicalPlayerAdapter;
 import ch.epfl.tchu.gui.GuiConstants;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class MainMenuClientController {
     String defaultIp = NetConstants.Network.DEFAULT_IP;
     int defaultPort = NetConstants.Network.DEFAULT_PORT;
-    @FXML private Button joinGame, configNgrok;
+    @FXML private Button joinGame, configNgrok, setFirstNumbers;
     @FXML private TextField IpField, port;
 
-    public void menuActions() throws UnknownHostException {
-        configNgrok.setOnMouseClicked(
-                e -> {
-                    scaleButton(configNgrok);
-                    GuiConstants.openNgrokConfigInfoStage();
-                });
-        joinGame.setOnMouseClicked(
-                e -> {
-                    scaleButton(joinGame);
-                    String ip;
-                    int port;
-                    if (IpField.getText().isEmpty()){
-                        IpField.setText(defaultIp);
-                        ip = defaultIp;
-                    } else ip = IpField.getText();
-                    if (this.port.getText().isEmpty()) {
-                        this.port.setText(String.valueOf(defaultPort));
-                        port = defaultPort;
-                    } else port = Integer.parseInt(this.port.getText());
-                    clientThread(ip, port).start();
-                });
+    public void setFirstNumbers() throws UnknownHostException {
+        IpField.setText("128.179.");
+    }
+
+    public void ngrokConfigAction() {
+        scaleButton(configNgrok);
+        GuiConstants.openNgrokConfigInfoStage();
+    }
+
+    public void joinGameAction() {
+        scaleButton(joinGame);
+        String ip;
+        int port;
+        if (IpField.getText().isEmpty()) {
+            IpField.setText(defaultIp);
+            ip = defaultIp;
+        } else ip = IpField.getText();
+
+        if (this.port.getText().isEmpty()) {
+            this.port.setText(String.valueOf(defaultPort));
+            port = defaultPort;
+        } else port = Integer.parseInt(this.port.getText());
+        clientThread(ip, port).start();
     }
 
     private void scaleButton(Button button) {
@@ -44,6 +44,7 @@ public class MainMenuClientController {
     }
 
     private Thread clientThread(String ip, int port) {
-        return new Thread(() -> new RemotePlayerClient(new GraphicalPlayerAdapter(), ip, port).run());
+        return new Thread(
+                () -> new RemotePlayerClient(new GraphicalPlayerAdapter(), ip, port).run());
     }
 }
